ff105479b8af9a3f500b7689070396695d2b0358
Florian de Boissieu <fdeboiss@gmail.com>
2021-02-15 16:54:26 +0000

    try error on testthat

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -17,5 +17,23 @@ test_that("Radiometric Filtering", {
   Input_Mask_File <- perform_radiometric_filtering(Image_Path = Input_Image_File, Mask_Path = Input_Mask_File,
                                                    Output_Dir = Output_Dir, TypePCA = TypePCA,
                                                    NDVI_Thresh = NDVI_Thresh, Blue_Thresh = Blue_Thresh,NIR_Thresh = NIR_Thresh)
-  expect_equal(Input_Mask_File, 'RESULTS/S2A_T33NUD_20180104_Subset/SPCA/ShadeMask_Update')
+  expect_equal(Input_Mask_File, 'RESULTS/S2A_T33NUD_20180104_Subset/SPCA/ShadeMask_Updat')
+})
+
+test_that("Radiometric Filtering", {
+  TypePCA <- 'SPCA'
+  FilterPCA <- TRUE
+
+  Excluded_WL <- c(0, 400)
+  Excluded_WL <- rbind(Excluded_WL, c(895, 1005))
+  Excluded_WL <- rbind(Excluded_WL, c(1180, 1480))
+  Excluded_WL <- rbind(Excluded_WL, c(1780, 2040))
+
+  Continuum_Removal <- TRUE
+  # Perform dimensionality reduction
+  print("PERFORM DIMENSIONALITY REDUCTION")
+  PCA_Output <- perform_PCA(Input_Image_File = Input_Image_File, Input_Mask_File = Input_Mask_File,
+                            Output_Dir = Output_Dir, TypePCA = TypePCA, FilterPCA=FilterPCA,
+                            nbCPU = nbCPU, MaxRAM = MaxRAM, Continuum_Removal = Continuum_Removal)
+
 })
