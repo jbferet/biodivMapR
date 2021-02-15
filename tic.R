c4db176c296909cfc45e7cbf5847194f9b0f616c
Florian de Boissieu <fdeboiss@gmail.com>
2021-02-15 15:16:43 +0000

    another try

--- tic.R
+++ tic.R
@@ -4,5 +4,5 @@ do_package_checks()
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
-  do_pkgdown()
+  do_pkgdown(deploy=T)
 }
